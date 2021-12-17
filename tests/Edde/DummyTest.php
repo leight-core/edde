8c2fa1feb987b229990e9c85a4eb94325e0ec247
Marek Hanzal <marek.hanzal@x32.cz>
2021-12-17 19:46:32 +0100

    Common: Prepared test environment

---
+++ tests/Edde/DummyTest.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+namespace Edde;
+
+use Edde\Phpunit\AbstractTestCase;
+
+class DummyTest extends AbstractTestCase {
+	public function testSomething() {
+		self::assertTrue(true);
+	}
+}
---
+++ tests/bootstrap.php
@@ -0,0 +1,5 @@
+<?php
+
+use Edde\Slim\SlimApp;
+
+SlimApp::create([]);

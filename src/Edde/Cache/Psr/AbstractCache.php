820b67c27388029c70678a6ff96cf4af9b4d4355
Marek Hanzal <marek.hanzal@x32.cz>
2022-01-18 14:40:36 +0100

    Common: Fixed a little butg

--- src/Edde/Cache/Psr/AbstractCache.php
+++ src/Edde/Cache/Psr/AbstractCache.php
@@ -3,7 +3,9 @@ declare(strict_types=1);
 
 namespace Edde\Cache\Psr;
 
+use Edde\Log\LoggerTrait;
 use Psr\SimpleCache\CacheInterface;
 
 abstract class AbstractCache implements CacheInterface {
+	use LoggerTrait;
 }

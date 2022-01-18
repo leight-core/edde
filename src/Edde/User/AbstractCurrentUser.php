d46c4fcb45de368017ac3bdba01d49ca725dd4ee
Marek Hanzal <marek.hanzal@x32.cz>
2022-01-18 14:16:52 +0100

    Common: Updated base user DTOs

--- src/Edde/User/AbstractCurrentUser.php
+++ src/Edde/User/AbstractCurrentUser.php
@@ -4,17 +4,20 @@ declare(strict_types=1);
 namespace Edde\User;
 
 use Edde\Dto\AbstractDto;
+use Edde\Role\Dto\RoleDto;
 use Edde\User\Dto\Settings\UserSettingsDto;
 
 abstract class AbstractCurrentUser extends AbstractDto {
-	/** @var string|void */
+	/** @var string */
 	public $id;
 	/** @var string */
 	public $name;
 	/** @var string */
-	public $login;
+	public $email;
 	/** @var string */
 	public $site;
 	/** @var UserSettingsDto|void */
 	public $settings;
+	/** @var RoleDto[] */
+	public $roles = [];
 }
--- src/Edde/User/AbstractUser.php
+++ src/Edde/User/AbstractUser.php
@@ -4,10 +4,20 @@ declare(strict_types=1);
 namespace Edde\User;
 
 use Edde\Dto\AbstractDto;
+use Edde\Role\Dto\RoleDto;
+use Edde\User\Dto\Settings\UserSettingsDto;
 
 abstract class AbstractUser extends AbstractDto {
 	/** @var string */
 	public $id;
 	/** @var string */
 	public $name;
+	/** @var string */
+	public $email;
+	/** @var string */
+	public $site;
+	/** @var UserSettingsDto|void */
+	public $settings;
+	/** @var RoleDto[] */
+	public $roles = [];
 }

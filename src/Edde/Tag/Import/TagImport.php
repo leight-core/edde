f703965b669bb0cf5a53112605604688cae537a3
Marek Hanzal <marek.hanzal@x32.cz>
2022-01-23 04:27:46 +0100

    Common: Added tag import

---
+++ src/Edde/Tag/Import/TagImport.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+namespace Edde\Tag\Import;
+
+use Edde\Import\AbstractImporter;
+use Edde\Tag\Repository\TagRepositoryTrait;
+
+class TagImport extends AbstractImporter {
+	use TagRepositoryTrait;
+
+	public function handle($item) {
+		return $this->tagRepository->ensure($item['code'], $item['group']);
+	}
+}

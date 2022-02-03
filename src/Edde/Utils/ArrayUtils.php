984648ff7ce8a51f70458b3711758295520bf382
Marek Hanzal <marek.hanzal@x32.cz>
2022-02-03 11:07:25 +0100

    Common: Added median

--- src/Edde/Utils/ArrayUtils.php
+++ src/Edde/Utils/ArrayUtils.php
@@ -4,9 +4,15 @@ declare(strict_types=1);
 namespace Edde\Utils;
 
 use Nette\Utils\Arrays;
+use function count;
 
 class ArrayUtils {
 	static public function mergeRecursive($arr1, $arr2): array {
 		return Arrays::mergeTree($arr1, $arr2);
+	}
+
+	static public function median(array $array): array {
+		$mid = floor((($count = count($array)) - 1) / 2);
+		return $count % 2 ? $array[$mid] : (($array[$mid] + $array[$mid + 1]) / 2);
 	}
 }

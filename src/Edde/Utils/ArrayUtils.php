bd2bc47b58766f0766e9719ecea0a8ea8fdef086
Marek Hanzal <marek.hanzal@x32.cz>
2022-02-03 11:13:47 +0100

    Common: ...

--- src/Edde/Utils/ArrayUtils.php
+++ src/Edde/Utils/ArrayUtils.php
@@ -6,18 +6,20 @@ namespace Edde\Utils;
 use Nette\Utils\Arrays;
 use function array_sum;
 use function count;
-use function max;
 
 class ArrayUtils {
 	static public function mergeRecursive($arr1, $arr2): array {
 		return Arrays::mergeTree($arr1, $arr2);
 	}
 
-	static public function avg(array $values): float {
-		return array_sum($values) / (max(count($values), 1));
+	static public function avg(array $values): ?float {
+		return empty($values) ? null : array_sum($values) / count($values);
 	}
 
-	static public function median(array $array): float {
+	static public function median(array $array): ?float {
+		if (empty($array)) {
+			return null;
+		}
 		$mid = floor((($count = count($array)) - 1) / 2);
 		return $count % 2 ? $array[$mid] : (($array[$mid] + $array[$mid + 1]) / 2);
 	}

ed48ef300d65fdc07a221fd915e49fb22110c517
Marek Hanzal <marek.hanzal@x32.cz>
2022-02-10 23:57:14 +0100

    Common: Enabled image conversion, yaay!

--- src/Edde/Image/ImageJobService.php
+++ src/Edde/Image/ImageJobService.php
@@ -18,9 +18,7 @@ use Edde\Repository\Exception\DuplicateEntryException;
 use Edde\Repository\Exception\RepositoryException;
 use Edde\Stream\FileStream;
 use Throwable;
-use function in_array;
 use function microtime;
-use function sprintf;
 use function str_replace;
 
 class ImageJobService extends AbstractJobService {
@@ -40,52 +38,48 @@ class ImageJobService extends AbstractJobService {
 	 * @throws Throwable
 	 */
 	protected function handle(IJob $job) {
-		static $allowed = [
-			'image/jpeg',
-			'image/png',
-		];
-
 		$progress = $job->getProgress();
 		$progress->onStart();
 
 		/** @var $file FileDto */
 		foreach ($this->fileMapper->map($this->fileRepository->execute((new Query())->withFilter(['pathEndLike' => '/image.raw']))) as $file) {
-			if (!in_array($file->mime, $allowed)) {
-				$this->logger->warning(sprintf('Image [%s] with invalid mime type [%s].', $file->path, $file->mime));
-				continue;
+			try {
+				$this->imageService->convert($file->native, 'jpeg');
+
+				$original = $this->fileService->store(
+					FileStream::openRead($file->native),
+					str_replace('/image.raw', '/original', $file->path),
+					$file->name,
+					null,
+					$file->user->id
+				);
+				/**
+				 * Prevent users for uploading some huuuge shits, so images will be kept in some sane dimensions.
+				 */
+				$this->imageService->resize($original->native, 2400, 2400);
+				$this->fileService->refresh($original->id);
+				$preview = $this->fileService->store(
+					FileStream::openRead($file->native),
+					str_replace('/image.raw', '', $file->path),
+					$file->name,
+					null,
+					$file->user->id
+				);
+				/**
+				 * Image preview will be quite small to keep the size small too.
+				 */
+				$this->imageService->resize($preview->native, 200, 200);
+				$this->fileService->refresh($preview->id);
+
+				$this->imageRepository->create($this->dtoService->fromArray(CreateDto::class, [
+					'gallery'    => str_replace('/image.raw', '', $file->path),
+					'originalId' => $original->id,
+					'previewId'  => $preview->id,
+					'userId'     => $file->user->id,
+				]));
+			} catch (Throwable $exception) {
+				$this->logger->error($exception);
 			}
-			$original = $this->fileService->store(
-				FileStream::openRead($file->native),
-				str_replace('/image.raw', '/original', $file->path),
-				$file->name,
-				null,
-				$file->user->id
-			);
-			/**
-			 * Prevent users for uploading some huuuge shits, so images will be kept in some sane dimensions.
-			 */
-			$this->imageService->resize($original->native, 2400, 2400);
-			$this->fileService->refresh($original->id);
-			$preview = $this->fileService->store(
-				FileStream::openRead($file->native),
-				str_replace('/image.raw', '', $file->path),
-				$file->name,
-				null,
-				$file->user->id
-			);
-			/**
-			 * Image preview will be quite small to keep the size small too.
-			 */
-			$this->imageService->resize($preview->native, 200, 200);
-			$this->fileService->refresh($preview->id);
-
-			$this->imageRepository->create($this->dtoService->fromArray(CreateDto::class, [
-				'gallery'    => str_replace('/image.raw', '', $file->path),
-				'originalId' => $original->id,
-				'previewId'  => $preview->id,
-				'userId'     => $file->user->id,
-			]));
-
 			/**
 			 * Mark raw file as stale (so it will be removed).
 			 */

63a7964f75c8bbb918912f08c7574246451456fb
Marek Hanzal <marek.hanzal@x32.cz>
2022-02-10 19:40:53 +0100

    Common: Image repo

--- src/Edde/Image/Repository/ImageRepository.php
+++ src/Edde/Image/Repository/ImageRepository.php
@@ -3,14 +3,28 @@ declare(strict_types=1);
 
 namespace Edde\Image\Repository;
 
+use ClanCats\Hydrahon\Query\Sql\Select;
 use DateTime;
 use Edde\Image\Dto\CreateDto;
+use Edde\Image\Dto\ImageFilterDto;
+use Edde\Query\Dto\Query;
 use Edde\Repository\AbstractRepository;
 use Edde\Repository\IRepository;
 
 class ImageRepository extends AbstractRepository {
 	public function __construct() {
 		parent::__construct(['stamp' => IRepository::ORDER_DESC]);
+	}
+
+	public function toQuery(Query $query): Select {
+		$select = parent::toQuery($query);
+
+		/** @var $filter ImageFilterDto */
+		$filter = $query->filter;
+
+		$this->toOrderBy($query->orderBy, $select);
+
+		return $select;
 	}
 
 	public function create(CreateDto $createDto) {

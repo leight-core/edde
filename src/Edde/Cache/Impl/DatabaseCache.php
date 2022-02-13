c50c63b86ad8eacc49e57b14dc203d826df2328c
Marek Hanzal <marek.hanzal@x32.cz>
2022-02-13 17:29:32 +0100

    Common: Fix?

--- src/Edde/Cache/Impl/DatabaseCache.php
+++ src/Edde/Cache/Impl/DatabaseCache.php
@@ -65,7 +65,7 @@ class DatabaseCache extends AbstractCache {
 
 	public function deleteMultiple($keys) {
 		try {
-			$this->cacheRepository->table()->delete()->where(['key' => $keys])->execute();
+			$this->cacheRepository->table()->delete()->where($keys, 'in', $keys)->execute();
 			return true;
 		} catch (Throwable $throwable) {
 			$this->logger->error($throwable);

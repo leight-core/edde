d1fee73b58be7ec91b7967c1e4663f3709ea1128
Marek Hanzal <marek.hanzal@x32.cz>
2022-02-14 05:07:32 +0100

    Common: One more loggggg

--- src/Edde/Image/ImageService.php
+++ src/Edde/Image/ImageService.php
@@ -5,6 +5,7 @@ namespace Edde\Image;
 
 use Edde\Log\LoggerTrait;
 use Imagick;
+use Throwable;
 use function sprintf;
 
 class ImageService implements IImageService {
@@ -16,12 +17,16 @@ class ImageService implements IImageService {
 	public function resize(string $file, int $width, int $height = null, string $copy = null): void {
 		$copy = $copy ?? $file;
 		$this->logger->debug(sprintf('Resizing image [%s] to [%s]; dimensions [%d x %d].', $file, $copy, $width, $height), ['tags' => [static::class]]);
-		$imagick = new Imagick($file);
-		$this->logger->debug('Image loaded', ['tags' => [static::class]]);
-		$imagick->resizeImage($width, $height, Imagick::FILTER_LANCZOS, 0, isset($height));
-		$this->logger->debug('Image resized', ['tags' => [static::class]]);
-		$imagick->writeImage($copy);
-		$this->logger->debug(sprintf('Image saved to [%s].', $copy), ['tags' => [static::class]]);
+		try {
+			$imagick = new Imagick($file);
+			$this->logger->debug('Image loaded', ['tags' => [static::class]]);
+			$imagick->resizeImage($width, $height, Imagick::FILTER_LANCZOS, 0, isset($height));
+			$this->logger->debug('Image resized', ['tags' => [static::class]]);
+			$imagick->writeImage($copy);
+			$this->logger->debug(sprintf('Image saved to [%s].', $copy), ['tags' => [static::class]]);
+		} catch (Throwable $throwable) {
+			$this->logger->error($throwable, ['tags' => [static::class]]);
+		}
 	}
 
 	/**
@@ -30,13 +35,17 @@ class ImageService implements IImageService {
 	public function convert(string $file, string $format, string $copy = null): void {
 		$copy = $copy ?? $file;
 
-		$this->logger->debug(sprintf('Converting [%s] to [%s], format [%s].', $file, $copy, $format), ['tags' => [static::class]]);
+		try {
+			$this->logger->debug(sprintf('Converting [%s] to [%s], format [%s].', $file, $copy, $format), ['tags' => [static::class]]);
 
-		$imagick = new Imagick($file);
-		$this->logger->debug('Image loaded', ['tags' => [static::class]]);
-		$imagick->setFormat($format);
-		$this->logger->debug('Format set', ['tags' => [static::class]]);
-		$imagick->writeImage($copy);
-		$this->logger->debug(sprintf('Image saved to [%s].', $copy), ['tags' => [static::class]]);
+			$imagick = new Imagick($file);
+			$this->logger->debug('Image loaded', ['tags' => [static::class]]);
+			$imagick->setFormat($format);
+			$this->logger->debug('Format set', ['tags' => [static::class]]);
+			$imagick->writeImage($copy);
+			$this->logger->debug(sprintf('Image saved to [%s].', $copy), ['tags' => [static::class]]);
+		} catch (Throwable $throwable) {
+			$this->logger->error($throwable, ['tags' => [static::class]]);
+		}
 	}
 }

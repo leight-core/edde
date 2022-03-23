b61584a0f8e8356abecc7dc1e785e379304498ef
Marek Hanzal <marek.hanzal@x32.cz>
2022-03-23 14:05:54 +0100

    Common: ...

--- src/Edde/Translation/Mapper/ToTranslationMapper.php
+++ src/Edde/Translation/Mapper/ToTranslationMapper.php
@@ -4,15 +4,16 @@ declare(strict_types=1);
 namespace Edde\Translation\Mapper;
 
 use Edde\Mapper\AbstractMapper;
+use Edde\Translation\Dto\TranslationDto;
 
 class ToTranslationMapper extends AbstractMapper {
 	public function item($item) {
-		return [
+		return $this->dtoService->fromArray(TranslationDto::class, [
 			'id'        => $item->id,
 			'language'  => $item->locale,
 			'label'     => $item->key,
 			'text'      => $item->translation,
 			'namespace' => 'translation',
-		];
+		]);
 	}
 }

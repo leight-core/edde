f6eaf51cd5193015363771349296e6d210eaf50a
Marek Hanzal <marek.hanzal@x32.cz>
2022-08-28 13:45:57 +0200

    Common: More logs X

--- src/Edde/Utils/ObjectUtils.php
+++ src/Edde/Utils/ObjectUtils.php
@@ -15,12 +15,23 @@ class ObjectUtils {
 	 * @return mixed
 	 */
 	static public function valueOf(object $object, ?array $value = null) {
+		/**
+		 * Get an initial object to jump on
+		 */
 		$item = $object;
 		foreach ($value ?? [] as $v) {
+			/**
+			 * If item is not an object (even if it's an array), do not continue; this is a strict
+			 * check to jump only on an objects, so one who is using this method must convert "object like" arrays
+			 * into an actual objects.
+			 */
 			if (!is_object($item)) {
 				break;
 			}
-			$item = $item->{$v};
+			/**
+			 * Move down in the object tree and check, if there is another value being requested down in the tree.
+			 */
+			$item = $item->{$v} ?? null;
 		}
 		return $item;
 	}

463a636b662940302ba09a4958d2e250659be825
Marek Hanzal <marek.hanzal@x32.cz>
2022-09-01 15:49:33 +0200

    Common: A little fix

--- src/Edde/Mapper/MapperUtilsTrait.php
+++ src/Edde/Mapper/MapperUtilsTrait.php
@@ -3,8 +3,15 @@ declare(strict_types=1);
 
 namespace Edde\Mapper;
 
+use DateTime;
+use function is_string;
+use const DATE_ATOM;
+
 trait MapperUtilsTrait {
 	public function isoDateNull($dateTime) {
-		return $dateTime ? $dateTime->format(DATE_ISO8601) : null;
+		if (is_string($dateTime)) {
+			$dateTime = new DateTime($dateTime);
+		}
+		return $dateTime ? $dateTime->format(DATE_ATOM) : null;
 	}
 }

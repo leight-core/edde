4621d048db6d3a0b08610461576beca3ef664e4e
Marek Hanzal <marek.hanzal@x32.cz>
2023-02-28 20:57:35 +0100

    ...

---
+++ src/Edde/Tag/Entity/TagEntity.php
@@ -0,0 +1,34 @@
+<?php
+declare(strict_types=1);
+
+namespace Edde\Tag\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Edde\Doctrine\AbstractUuidEntity;
+
+/**
+ * @ORM\Entity()
+ * @ORM\Table(name="tag")
+ */
+class TagEntity extends AbstractUuidEntity {
+	/**
+	 * @ORM\Column(type="string", length=128)
+	 * @var string
+	 */
+	public $code;
+	/**
+	 * @ORM\Column(type="string", length=128, name="label")
+	 * @var string
+	 */
+	public $tag;
+	/**
+	 * @ORM\Column(type="string", length=128, nullable=true)
+	 * @var string|null
+	 */
+	public $group;
+	/**
+	 * @ORM\Column(type="integer", options={"default": 0})
+	 * @var int
+	 */
+	public $sort = 0;
+}

1d63af4f4eb68d44b27ba65a955b271d0c7c30eb
Marek Hanzal <marek.hanzal@x32.cz>
2023-02-28 20:34:28 +0100

    ...

---
+++ src/Edde/Doctrine/AbstractEntity.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+
+namespace Edde\Doctrine;
+
+/**
+ * Just an optional entity root.
+ */
+abstract class AbstractEntity {
+}
--- src/Edde/Doctrine/AbstractUuidEntity.php
+++ src/Edde/Doctrine/AbstractUuidEntity.php
@@ -5,7 +5,7 @@ namespace Edde\Doctrine;
 
 use Doctrine\ORM\Mapping as ORM;
 
-abstract class AbstractUuidEntity {
+abstract class AbstractUuidEntity extends AbstractEntity {
 	/**
 	 * @ORM\Id()
 	 * @ORM\Column(type="uuid", unique=true)

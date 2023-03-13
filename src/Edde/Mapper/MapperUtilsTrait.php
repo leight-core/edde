3df0a5eecf14d58f167789d0d4c70a5134973a5f
Marek Hanzal <marek.hanzal@x32.cz>
2023-03-13 19:51:53 +0100

    ...

--- src/Edde/Mapper/MapperUtilsTrait.php
+++ src/Edde/Mapper/MapperUtilsTrait.php
@@ -4,15 +4,14 @@ declare(strict_types=1);
 namespace Edde\Mapper;
 
 use DateTime;
-use DateTimeZone;
 use function is_string;
 use const DATE_ATOM;
 
 trait MapperUtilsTrait {
 	public function isoDateNull($dateTime) {
 		if (is_string($dateTime)) {
-			$dateTime = new DateTime($dateTime);
+			$dateTime = new DateTime($dateTime, 'UTC');
 		}
-		return $dateTime ? $dateTime->setTimezone(new DateTimeZone('UTC'))->format(DATE_ATOM) : null;
+		return $dateTime ? $dateTime->format(DATE_ATOM) : null;
 	}
 }

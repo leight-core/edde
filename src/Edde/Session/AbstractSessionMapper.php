70bf254ca89613d4ec31a32408e26e0c9842474f
Marek Hanzal <marek.hanzal@x32.cz>
2023-07-13 15:26:22 +0200

    Removing obsolete stuff

--- src/Edde/Session/AbstractSessionMapper.php
+++
@@ -1,9 +0,0 @@
-<?php
-declare(strict_types=1);
-
-namespace Edde\Session;
-
-use Edde\Mapper\AbstractMapper;
-
-abstract class AbstractSessionMapper extends AbstractMapper implements ISessionMapper {
-}

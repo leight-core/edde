501998e282719dd2bafc8f27994231fbbe063e44
Marek Hanzal <marek.hanzal@x32.cz>
2023-07-13 17:03:14 +0200

    Preparing SDK generator

--- src/Edde/Sdk/Sdk.php
+++ src/Edde/Sdk/Sdk.php
@@ -4,12 +4,15 @@ declare(strict_types=1);
 namespace Edde\Sdk;
 
 use Edde\Container\ContainerTrait;
+use Edde\File\FileServiceTrait;
 
 class Sdk {
 	use ContainerTrait;
+	use FileServiceTrait;
 
 	public function generate(?string $output = null) {
 		$this->container->injectOn($generator = new RpcHandlerGenerator());
+		$this->fileService->remove($output);
 		$generator
 			->withOutput($output ?? sprintf('%s/sdk', getcwd()))
 			->generate();

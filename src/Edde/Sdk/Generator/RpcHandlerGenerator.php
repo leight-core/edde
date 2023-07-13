cb72f8766384f4fb44c620a9655612b6b6998635
Marek Hanzal <marek.hanzal@x32.cz>
2023-07-13 20:50:48 +0200

    Preparing SDK generator

--- src/Edde/Sdk/Generator/RpcHandlerGenerator.php
+++ src/Edde/Sdk/Generator/RpcHandlerGenerator.php
@@ -19,8 +19,8 @@ class RpcHandlerGenerator extends AbstractGenerator {
 		$queryExport = $this->container->injectOn(new QueryExport());
 		$mutationExport = $this->container->injectOn(new MutationExport());
 
-		$rpcOutput = sprintf('%s/rpc', $this->output);
-		$exportOutput = sprintf('%s/$export', $this->output);
+		$rpcOutput = sprintf('%s/src/rpc', $this->output);
+		$exportOutput = sprintf('%s/src/$export', $this->output);
 		$this->mkdir($rpcOutput);
 		$this->mkdir($exportOutput);
 

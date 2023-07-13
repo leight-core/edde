388877478e965c16c61a3b8ed15ca4b555c65f24
Marek Hanzal <marek.hanzal@x32.cz>
2023-07-13 15:15:57 +0200

    Added AbstractSessionMapper

---
+++ src/Edde/Auth/Mapper/AbstractSessionMapper.php
@@ -0,0 +1,9 @@
+<?php
+declare(strict_types=1);
+
+namespace Edde\Auth\Mapper;
+
+use Edde\Mapper\AbstractMapper;
+
+abstract class AbstractSessionMapper extends AbstractMapper implements ISessionMapper {
+}

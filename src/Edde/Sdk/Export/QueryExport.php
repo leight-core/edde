26fbe852dc8ec7d486c513af0d11349de7f7f38c
Marek Hanzal <marek.hanzal@x32.cz>
2023-07-13 20:52:52 +0200

    Preparing SDK generator

--- src/Edde/Sdk/Export/QueryExport.php
+++ src/Edde/Sdk/Export/QueryExport.php
@@ -5,6 +5,36 @@ namespace Edde\Sdk\Export;
 
 class QueryExport extends AbstractRpcExport {
 	public function export(): ?string {
-		return null;
+		$import = [
+			'import {withQuery}  from "@leight/rpc-client";',
+		];
+
+		$rpcName = $this->handler->getName();
+		$schemaExport = new SchemaExport();
+		$requestSchema = 'undefined';
+		$responseSchema = 'undefined';
+
+		if (($name = $this->handler->getRequestSchema()) && $schema = $this->schemaLoader->load($name)) {
+			$import[] = sprintf('import {%s} from "../schema/%s";', $requestSchema = $schemaExport->getSchemaName($schema) . 'Schema', $requestSchema);
+		}
+		if (($name = $this->handler->getResponseSchema()) && $schema = $this->schemaLoader->load($name)) {
+			$import[] = sprintf('import {%s} from "../schema/%s";', $responseSchema = $schemaExport->getSchemaName($schema) . 'Schema', $responseSchema);
+		}
+
+		$export = [
+			$this->toExport($import, "\n"),
+		];
+
+		$export[] = sprintf('
+export const with%s = withQuery({
+	service: "%s",
+	schema:  {
+		request:  %s,
+		response: %s,
+	},
+});
+		', $rpcName, $this->escapeHandlerName(get_class($this->handler)), $requestSchema, $responseSchema);
+
+		return $this->toExport($export);
 	}
 }

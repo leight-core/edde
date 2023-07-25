e9bea1a00b6c34f6f99a4057738d76e1a3156241
Marek Hanzal <marek.hanzal@x32.cz>
2023-07-25 18:41:22 +0200

    ,,,

--- src/Edde/Rpc/Service/RpcService.php
+++ src/Edde/Rpc/Service/RpcService.php
@@ -44,9 +44,9 @@ class RpcService {
 				$result = $service->handle(
 					$requestName ? $this->smartService->from((object)$bulk->get('data')->get(), $requestName) : SmartDto::ofDummy()
 				);
-				$response[$id] = $result ? (object)[
-					'data' => iterator_to_array($result->getValues()),
-				] : null;
+				$response[$id] = (object)[
+					'data' => $result ? iterator_to_array($result->getValues()) : null,
+				];
 			} catch (NotFoundException $exception) {
 				$this->logger->error($exception);
 				$response[$id] = (object)[

5357dba15f3e451cada5f9a8b21d5e0348b3887e
Marek Hanzal <marek.hanzal@x32.cz>
2023-07-28 11:13:52 +0200

    ...

---
+++ src/Edde/User/Mapper/AbstractUserMapper.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+namespace Edde\User\Mapper;
+
+use Edde\Bridge\User\UserDto;
+use Edde\Mapper\AbstractMapper;
+
+abstract class AbstractUserMapper extends AbstractMapper implements IUserMapper {
+	public function item($item, $params = null) {
+		return $this->dtoService->fromArray(UserDto::class, $this->toUser($item));
+	}
+
+	abstract protected function toUser($item): array;
+}

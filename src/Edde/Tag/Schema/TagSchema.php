d7a233c484564ffc59f3c2a77bd2cceb02303200
Marek Hanzal <marek.hanzal@x32.cz>
2023-08-08 16:25:10 +0200

    ...

---
+++ src/Edde/Tag/Schema/TagSchema.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+namespace Edde\Tag\Schema;
+
+abstract class TagSchema {
+	abstract function id(): string;
+
+	abstract function code(): string;
+
+	abstract function tag(): string;
+
+	abstract function group(): ?string;
+
+	abstract function sort(): ?int;
+}

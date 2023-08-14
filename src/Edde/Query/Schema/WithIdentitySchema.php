660c02aa2750f13fcdf1fc5cdadceb0ec2076560
Marek Hanzal <marek.hanzal@x32.cz>
2023-08-14 15:06:51 +0200

    ...

--- src/Edde/Query/Schema/WithIdentitySchema.php
+++ src/Edde/Query/Schema/WithIdentitySchema.php
@@ -4,5 +4,11 @@ declare(strict_types=1);
 namespace Edde\Query\Schema;
 
 abstract class WithIdentitySchema {
+	const meta = [
+		'import' => [
+			'WithIdentitySchema' => '@leight/query',
+		],
+	];
+
 	abstract public function id(): string;
 }

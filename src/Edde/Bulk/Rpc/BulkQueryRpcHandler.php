e0aac2e101fbac982f150b38caeb6e13488c83f4
Marek Hanzal <marek.hanzal@x32.cz>
2023-08-14 19:35:13 +0200

    ...

--- src/Edde/Bulk/Rpc/BulkQueryRpcHandler.php
+++ src/Edde/Bulk/Rpc/BulkQueryRpcHandler.php
@@ -3,14 +3,20 @@ declare(strict_types=1);
 
 namespace Edde\Bulk\Rpc;
 
+use Edde\Bulk\Mapper\BulkDtoMapperTrait;
+use Edde\Bulk\Repository\BulkRepositoryTrait;
 use Edde\Bulk\Schema\BulkSchema;
 use Edde\Dto\SmartDto;
 use Edde\Rpc\AbstractRpcHandler;
 
 class BulkQueryRpcHandler extends AbstractRpcHandler {
+	use BulkRepositoryTrait;
+	use BulkDtoMapperTrait;
+
 	protected $responseSchema = BulkSchema::class;
 	protected $responseSchemaArray = true;
 
 	public function handle(SmartDto $request) {
+		return $this->bulkDtoMapper->map($this->bulkRepository->withQuery('b', $request));
 	}
 }

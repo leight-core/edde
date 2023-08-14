a6458f2c750cd65f2249fa13754af26daae830d5
Marek Hanzal <marek.hanzal@x32.cz>
2023-08-14 21:24:26 +0200

    ...

--- src/Edde/Sdk/Export/SchemaExport.php
+++ src/Edde/Sdk/Export/SchemaExport.php
@@ -23,7 +23,7 @@ class SchemaExport extends AbstractExport {
 
 	protected function toZod(ISchema $schema): string {
 		if ($orderBy = $schema->getMeta('orderBy')) {
-			return sprintf('z.record(z.enum(["asc", "desc"], z.enum([%s])));', implode(', ', array_map(function ($item) {
+			return sprintf('z.record(z.enum(["asc", "desc"], z.enum([%s])))', implode(', ', array_map(function ($item) {
 				return sprintf('"%s"', $item);
 			}, $orderBy)));
 		}

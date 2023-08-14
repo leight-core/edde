aaf39c985bcf4e66a640dde4c418b192d42992ae
Marek Hanzal <marek.hanzal@x32.cz>
2023-08-14 19:50:57 +0200

    ...

--- src/Edde/Bulk/Mapper/BulkDtoMapper.php
+++ src/Edde/Bulk/Mapper/BulkDtoMapper.php
@@ -3,9 +3,11 @@ declare(strict_types=1);
 
 namespace Edde\Bulk\Mapper;
 
+use Edde\Bulk\Schema\BulkSchema;
 use Edde\Mapper\AbstractMapper;
 
 class BulkDtoMapper extends AbstractMapper {
 	public function item($item, $params = null) {
+		return $this->smartService->from($item, BulkSchema::class);
 	}
 }

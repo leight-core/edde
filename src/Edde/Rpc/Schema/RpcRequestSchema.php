f8e77f20371f5df5b4d798ec5067018b6337ffb2
Marek Hanzal <marek.hanzal@x32.cz>
2023-08-15 10:23:21 +0200

    ...

--- src/Edde/Rpc/Schema/RpcRequestSchema.php
+++ src/Edde/Rpc/Schema/RpcRequestSchema.php
@@ -3,8 +3,8 @@ declare(strict_types=1);
 
 namespace Edde\Rpc\Schema;
 
-abstract class RpcRequestSchema {
-	abstract public function service(): string;
+interface RpcRequestSchema {
+	function service(): string;
 
-	abstract public function data(): array;
+	function data(): ?array;
 }

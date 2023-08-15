257062b0a42ee0c34e4b96e9d66e1b559e2cb735
Marek Hanzal <marek.hanzal@x32.cz>
2023-08-15 15:29:50 +0200

    ...

--- src/Edde/Sdk/Export/MutationExport.php
+++ src/Edde/Sdk/Export/MutationExport.php
@@ -7,6 +7,7 @@ class MutationExport extends AbstractRpcExport {
 	public function export(): ?string {
 		$import = [
 			'import {withMutation} from "@leight/rpc";',
+			'import {z}            from "@leight/utils";',
 		];
 
 		$rpcName = $this->handler->getName();

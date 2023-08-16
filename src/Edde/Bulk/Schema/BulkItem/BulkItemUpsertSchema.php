09af14f1c23ce62fa4cbc48de13073af47f2153f
Marek Hanzal <marek.hanzal@x32.cz>
2023-08-16 10:53:19 +0200

    Version bump

--- src/Edde/Bulk/Schema/BulkItem/BulkItemUpsertSchema.php
+++ src/Edde/Bulk/Schema/BulkItem/BulkItemUpsertSchema.php
@@ -4,9 +4,9 @@ declare(strict_types=1);
 namespace Edde\Bulk\Schema\BulkItem;
 
 interface BulkItemUpsertSchema {
-	function create(): ?BulkItemCreateSchema;
+	function create($load = true): ?BulkItemCreateSchema;
 
-	function update(): ?BulkItemCreateSchema;
+	function update($load = true): ?BulkItemCreateSchema;
 
-	function filter(): BulkItemFilterSchema;
+	function filter($load = true): BulkItemFilterSchema;
 }

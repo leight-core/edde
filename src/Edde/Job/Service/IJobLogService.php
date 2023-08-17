5f0aed9fd0e35f901a995c2f23ab4322dea6c260
Marek Hanzal <marek.hanzal@x32.cz>
2023-08-17 21:57:47 +0200

    Version bump

--- src/Edde/Job/Service/IJobLogService.php
+++ src/Edde/Job/Service/IJobLogService.php
@@ -3,6 +3,21 @@ declare(strict_types=1);
 
 namespace Edde\Job\Service;
 
+use Edde\Dto\Exception\SmartDtoException;
+use ReflectionException;
+
 interface IJobLogService {
+	/**
+	 * @param string      $jobId
+	 * @param int         $level
+	 * @param string      $message
+	 * @param             $context
+	 * @param string|null $type
+	 * @param string|null $reference
+	 *
+	 * @return void
+	 * @throws ReflectionException
+	 * @throws SmartDtoException
+	 */
 	public function log(string $jobId, int $level, string $message, $context = null, string $type = null, string $reference = null): void;
 }

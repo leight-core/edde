3b545942febe99daf5993d9d17a35d7b22723b3f
Marek Hanzal <marek.hanzal@x32.cz>
2023-08-17 19:27:35 +0200

    Version bump

--- src/Edde/Job/Entity/JobEntity.php
+++ src/Edde/Job/Entity/JobEntity.php
@@ -11,4 +11,74 @@ use Edde\Doctrine\AbstractUuidEntity;
  * @ORM\Table(name="z_job")
  */
 class JobEntity extends AbstractUuidEntity {
+	/**
+	 * @ORM\Column(type="string", length=256)
+	 * @var string
+	 */
+	public $service;
+	/**
+	 * @ORM\Column(type="integer")
+	 * @var string
+	 */
+	public $status;
+	/**
+	 * @ORM\Column(type="integer")
+	 * @var string
+	 */
+	public $total;
+	/**
+	 * @ORM\Column(type="float")
+	 * @var string
+	 */
+	public $progress;
+	/**
+	 * @ORM\Column(type="integer")
+	 * @var string
+	 */
+	public $successCount;
+	/**
+	 * @ORM\Column(type="integer")
+	 * @var string
+	 */
+	public $errorCount;
+	/**
+	 * @ORM\Column(type="integer")
+	 * @var string
+	 */
+	public $skipCount;
+	/**
+	 * @ORM\Column(type="text", nullable=true)
+	 * @var string
+	 */
+	public $request;
+	/**
+	 * @ORM\Column(type="string", length=256, nullable=true)
+	 * @var string
+	 */
+	public $requestSchema;
+	/**
+	 * @ORM\Column(type="text", nullable=true)
+	 * @var string
+	 */
+	public $response;
+	/**
+	 * @ORM\Column(type="string", length=256, nullable=true)
+	 * @var string
+	 */
+	public $responseSchema;
+	/**
+	 * @ORM\Column(type="datetime")
+	 * @var string
+	 */
+	public $started;
+	/**
+	 * @ORM\Column(type="datetime", nullable=true)
+	 * @var string
+	 */
+	public $finished;
+	/**
+	 * @ORM\Column(type="string", length=36)
+	 * @var string
+	 */
+	public $userId;
 }

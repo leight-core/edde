f3b1085dc7fdb3cf4d9172355b0363b9d6b49d41
Marek Hanzal <marek.hanzal@x32.cz>
2023-08-20 17:32:18 +0200

    ...

--- src/Edde/Bulk/Schema/Bulk/Query/BulkQuerySchema.php
+++ src/Edde/Bulk/Schema/Bulk/Query/BulkQuerySchema.php
@@ -6,6 +6,14 @@ namespace Edde\Bulk\Schema\Bulk\Query;
 use Edde\Query\Schema\CursorSchema;
 
 interface BulkQuerySchema {
+	const meta = [
+		'import' => [
+			'BulkQuerySchema'       => '@leight/bulk',
+			'type IBulkQuerySchema' => '@leight/bulk',
+			'type IBulkQuery'       => '@leight/bulk',
+		],
+	];
+
 	function filter(): ?BulkFilterSchema;
 
 	function orderBy(): ?BulkOrderBySchema;

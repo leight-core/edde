96b55739d82fd80d5c3736aff9f7b087c6cb649c
Marek Hanzal <marek.hanzal@x32.cz>
2023-08-20 17:28:27 +0200

    ...

--- src/Edde/Bulk/Schema/Bulk/BulkValuesSchema.php
+++ src/Edde/Bulk/Schema/Bulk/BulkValuesSchema.php
@@ -4,5 +4,13 @@ declare(strict_types=1);
 namespace Edde\Bulk\Schema\Bulk;
 
 interface BulkValuesSchema {
+	const meta = [
+		'import' => [
+			'type IBulkValuesSchema' => '@leight/bulk',
+			'type IBulkValues'       => '@leight/bulk',
+			'BulkValuesSchema'       => '@leight/bulk',
+		],
+	];
+
 	function name(): string;
 }
--- src/Edde/Bulk/Schema/BulkItem/Query/BulkItemQuerySchema.php
+++ src/Edde/Bulk/Schema/BulkItem/Query/BulkItemQuerySchema.php
@@ -6,6 +6,14 @@ namespace Edde\Bulk\Schema\BulkItem\Query;
 use Edde\Query\Schema\CursorSchema;
 
 interface BulkItemQuerySchema {
+	const meta = [
+		'import' => [
+			'type IBulkItemQuery'       => '@leight/bulk',
+			'type IBulkItemQuerySchema' => '@leight/bulk',
+			'BulkItemQuerySchema'       => '@leight/bulk',
+		],
+	];
+
 	function filter($load = true): ?BulkItemFilterSchema;
 
 	function orderBy($load = true): ?BulkItemOrderBySchema;

e77425cab21ee9943abd3b5a009eaec9e83faf8b
Marek Hanzal <marek.hanzal@x32.cz>
2023-08-20 17:38:12 +0200

    ...

--- src/Edde/Query/Schema/CursorSchema.php
+++ src/Edde/Query/Schema/CursorSchema.php
@@ -4,6 +4,14 @@ declare(strict_types=1);
 namespace Edde\Query\Schema;
 
 interface CursorSchema {
+	const meta = [
+		'import' => [
+			'type ICursorSchema' => '@leight/query',
+			'type ICursor'       => '@leight/query',
+			'CursorSchema'       => '@leight/query',
+		],
+	];
+
 	function page(): int;
 
 	function size(): int;

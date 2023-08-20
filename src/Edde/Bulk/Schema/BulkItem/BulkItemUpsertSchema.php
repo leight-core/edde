c4484e14cae09b98fedb34dbc5211247193915e1
Marek Hanzal <marek.hanzal@x32.cz>
2023-08-20 17:25:33 +0200

    ...

--- src/Edde/Bulk/Schema/BulkItem/BulkItemUpsertSchema.php
+++ src/Edde/Bulk/Schema/BulkItem/BulkItemUpsertSchema.php
@@ -6,6 +6,14 @@ namespace Edde\Bulk\Schema\BulkItem;
 use Edde\Bulk\Schema\BulkItem\Query\BulkItemFilterSchema;
 
 interface BulkItemUpsertSchema {
+	const meta = [
+		'import' => [
+			'BulkItemUpsertSchema'       => '@leight/bulk',
+			'type IBulkItemUpsertSchema' => '@leight/bulk',
+			'type IBulkItemUpsert'       => '@leight/bulk',
+		],
+	];
+
 	function create($load = true): ?BulkItemCreateSchema;
 
 	function update($load = true): ?BulkItemCreateSchema;

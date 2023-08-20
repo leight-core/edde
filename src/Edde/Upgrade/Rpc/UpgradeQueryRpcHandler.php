cd3fde3a674eedd5b09e15754a946ec25a70fb9d
Marek Hanzal <marek.hanzal@x32.cz>
2023-08-20 13:03:49 +0200

    ...

--- src/Edde/Upgrade/Rpc/UpgradeQueryRpcHandler.php
+++ src/Edde/Upgrade/Rpc/UpgradeQueryRpcHandler.php
@@ -8,9 +8,11 @@ use Edde\Rpc\AbstractRpcHandler;
 use Edde\Upgrade\Schema\UpgradeFilterSchema;
 use Edde\Upgrade\Schema\UpgradeOrderBySchema;
 use Edde\Upgrade\Schema\UpgradeQuerySchema;
+use Edde\Upgrade\Schema\UpgradeSchema;
 
 class UpgradeQueryRpcHandler extends AbstractRpcHandler {
 	protected $requestSchema = UpgradeQuerySchema::class;
+	protected $responseSchema = UpgradeSchema::class;
 	protected $responseSchemaArray = true;
 	protected $orderBySchema = UpgradeOrderBySchema::class;
 	protected $filterSchema = UpgradeFilterSchema::class;

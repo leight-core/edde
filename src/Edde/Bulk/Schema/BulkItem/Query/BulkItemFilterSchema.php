2aeb08b3f0665a5c0c82ed984ed0da3573038025
Marek Hanzal <marek.hanzal@x32.cz>
2023-08-21 09:52:39 +0200

    ...

--- src/Edde/Bulk/Schema/BulkItem/Query/BulkItemFilterSchema.php
+++ src/Edde/Bulk/Schema/BulkItem/Query/BulkItemFilterSchema.php
@@ -3,14 +3,18 @@ declare(strict_types=1);
 
 namespace Edde\Bulk\Schema\BulkItem\Query;
 
+use Edde\Dto\Mapper\ExportMapper;
 use Edde\Query\Schema\FilterSchema;
 
 interface BulkItemFilterSchema extends FilterSchema {
 	const meta = [
-		'import' => [
+		'import'           => [
 			'type IBulkItemFilter'       => '@leight/bulk',
 			'type IBulkItemFilterSchema' => '@leight/bulk',
 			'BulkItemFilterSchema'       => '@leight/bulk',
+		],
+		ExportMapper::META => [
+			'bulkId' => ExportMapper::CONVERT_SNAKE,
 		],
 	];
 

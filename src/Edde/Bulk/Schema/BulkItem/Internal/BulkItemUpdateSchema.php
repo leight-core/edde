a4c1a50416c5c5bbc2bc841d00e760da1616d66c
Marek Hanzal <marek.hanzal@x32.cz>
2023-08-21 14:16:30 +0200

    ...

--- src/Edde/Bulk/Schema/BulkItem/Internal/BulkItemUpdateSchema.php
+++ src/Edde/Bulk/Schema/BulkItem/Internal/BulkItemUpdateSchema.php
@@ -3,6 +3,12 @@ declare(strict_types=1);
 
 namespace Edde\Bulk\Schema\BulkItem\Internal;
 
+use Edde\Utils\Mapper\JsonInputMapper;
+
 interface BulkItemUpdateSchema extends BulkItemCreateSchema {
 	const partial = true;
+
+	function response(
+		$input = JsonInputMapper::class
+	): string;
 }

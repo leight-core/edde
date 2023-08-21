311e386a6ab3c041b3021447facd72454e369eee
Marek Hanzal <marek.hanzal@x32.cz>
2023-08-21 13:41:17 +0200

    ...

--- src/Edde/Job/Schema/JobLock/JobLockSchema.php
+++ src/Edde/Job/Schema/JobLock/JobLockSchema.php
@@ -4,9 +4,16 @@ declare(strict_types=1);
 namespace Edde\Job\Schema\JobLock;
 
 use Edde\Database\Schema\UuidSchema;
+use Edde\Dto\Mapper\ImportMapper;
 use Edde\Dto\Mapper\ITypeMapper;
 
 interface JobLockSchema extends UuidSchema {
+	const meta = [
+		ImportMapper::META => [
+			'job_id' => ImportMapper::CONVERT_CAMEL,
+		],
+	];
+
 	function jobId(): string;
 
 	function name(): string;
--- src/Edde/Job/Schema/JobLog/JobLogSchema.php
+++ src/Edde/Job/Schema/JobLog/JobLogSchema.php
@@ -4,9 +4,16 @@ declare(strict_types=1);
 namespace Edde\Job\Schema\JobLog;
 
 use Edde\Database\Schema\UuidSchema;
+use Edde\Dto\Mapper\ImportMapper;
 use Edde\Dto\Mapper\ITypeMapper;
 
 interface JobLogSchema extends UuidSchema {
+	const meta = [
+		ImportMapper::META => [
+			'job_id' => ImportMapper::CONVERT_CAMEL,
+		],
+	];
+
 	function jobId(): string;
 
 	function level(): int;

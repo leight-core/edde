09b811c21864ff13a898dd766d65d189456709d2
Marek Hanzal <marek.hanzal@x32.cz>
2023-08-22 14:15:03 +0200

    ...

--- src/Edde/Job/Schema/Job/Query/JobFilterSchema.php
+++ src/Edde/Job/Schema/Job/Query/JobFilterSchema.php
@@ -6,4 +6,9 @@ namespace Edde\Job\Schema\Job\Query;
 use Edde\Query\Schema\FilterSchema;
 
 interface JobFilterSchema extends FilterSchema {
+	function status(): ?int;
+
+	function service(): ?string;
+
+	function services($array = true): ?string;
 }

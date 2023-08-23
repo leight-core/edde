6e25a079a1ef3f98c4f50b01cdb18467d8768cf9
Marek Hanzal <marek.hanzal@x32.cz>
2023-08-23 13:41:30 +0200

    ...

--- src/Edde/Database/Schema/CountSchema.php
+++ src/Edde/Database/Schema/CountSchema.php
@@ -4,5 +4,13 @@ declare(strict_types=1);
 namespace Edde\Database\Schema;
 
 interface CountSchema {
+    const meta = [
+        'import' => [
+            'CountSchema'       => '@pico/source',
+            'type ICount'       => '@pico/source',
+            'type ICountSchema' => '@pico/source',
+        ],
+    ];
+
     function count(): int;
 }

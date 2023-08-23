45ab783ee6fced5791a46901b94080c0be3c9803
Marek Hanzal <marek.hanzal@x32.cz>
2023-08-23 16:59:05 +0200

    ...

--- src/Edde/Job/Repository/JobRepository.php
+++ src/Edde/Job/Repository/JobRepository.php
@@ -11,45 +11,46 @@ use Edde\Job\Schema\Job\Query\JobQuerySchema;
 use Edde\Job\Schema\JobStatus;
 
 class JobRepository extends AbstractRepository {
-	use JobLogRepositoryTrait;
+    use JobLogRepositoryTrait;
 
-	public function __construct() {
-		parent::__construct(JobSchema::class);
-		$this->orderBy = [
-			'$.created' => 'desc',
-		];
-		$this->fulltextOf = [
-			'$.id',
-			'$.service',
-			'$.params',
-		];
-		$this->matchOf = [
-			'$.user_id',
-			'$.service',
-		];
-	}
+    public function __construct() {
+        parent::__construct(JobSchema::class);
+        $this->orderBy = [
+            '$.created' => 'desc',
+        ];
+        $this->fulltextOf = [
+            'id'      => '$.id',
+            'service' => '$.service',
+            'params'  => '$.params',
+        ];
+        $this->matchOf = [
+            'id'      => '$.id',
+            'userId'  => '$.user_id',
+            'service' => '$.service',
+        ];
+    }
 
-	protected function applyWhere(SmartDto $filter, SmartDto $query, Query $builder): void {
-		parent::applyWhere($filter, $query, $builder);
-		$filter->knownWithValue('id') && $this->matchOf($builder, '$.id', $filter->getValue('id'));
-		$filter->knownWithValue('userId') && $this->matchOf($builder, '$.user_id', $filter->getValue('userId'));
-		$filter->knownWithValue('status') && $this->matchOf($builder, '$.status', $filter->getValue('status'));
-		$filter->knownWithValue('statusIn') && $this->matchOfIn($builder, '$.status', $filter->getValue('statusIn'));
-		$filter->knownWithValue('service') && $this->matchOf($builder, '$.service', $filter->getValue('service'));
-		$filter->knownWithValue('serviceIn') && $this->matchOfIn($builder, '$.service', $filter->getValue('serviceIn'));
-		$filter->knownWithValue('params') && $this->fulltextOf($builder, '$.params', $filter->getValue('params'));
-	}
+    protected function applyWhere(SmartDto $filter, SmartDto $query, Query $builder): void {
+        parent::applyWhere($filter, $query, $builder);
+        $filter->knownWithValue('id') && $this->matchOf($builder, '$.id', $filter->getValue('id'));
+        $filter->knownWithValue('userId') && $this->matchOf($builder, '$.user_id', $filter->getValue('userId'));
+        $filter->knownWithValue('status') && $this->matchOf($builder, '$.status', $filter->getValue('status'));
+        $filter->knownWithValue('statusIn') && $this->matchOfIn($builder, '$.status', $filter->getValue('statusIn'));
+        $filter->knownWithValue('service') && $this->matchOf($builder, '$.service', $filter->getValue('service'));
+        $filter->knownWithValue('serviceIn') && $this->matchOfIn($builder, '$.service', $filter->getValue('serviceIn'));
+        $filter->knownWithValue('params') && $this->fulltextOf($builder, '$.params', $filter->getValue('params'));
+    }
 
-	public function cleanup(): void {
-		$this->deleteWith(
-			$this->smartService->from(
-				[
-					'filter' => [
-						'status' => JobStatus::JOB_SUCCESS,
-					],
-				],
-				JobQuerySchema::class
-			)
-		);
-	}
+    public function cleanup(): void {
+        $this->deleteWith(
+            $this->smartService->from(
+                [
+                    'filter' => [
+                        'status' => JobStatus::JOB_SUCCESS,
+                    ],
+                ],
+                JobQuerySchema::class
+            )
+        );
+    }
 }

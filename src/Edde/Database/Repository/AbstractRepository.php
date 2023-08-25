96bc3cd600298edd6bf7e23cd6993fe7c800ebfe
Marek Hanzal <marek.hanzal@x32.cz>
2023-08-25 14:22:56 +0200

    ...

--- src/Edde/Database/Repository/AbstractRepository.php
+++ src/Edde/Database/Repository/AbstractRepository.php
@@ -142,7 +142,8 @@ abstract class AbstractRepository extends AbstractMapper implements IRepository 
     public function total(SmartDto $query): SmartDto {
         $builder = $this->applyQueryBuilder($this->queryOf());
         $builder
-            ->select(['count' => $builder->func()->count($this->field("$.{$this->id}"))], true)
+            ->select(['count' => $builder->func()->count($field = $this->field("$.$this->id"))], true)
+            ->distinct($field)
             ->from($this->table);
         $query->knownWithValue('filter') && $this->applyWhere($query->getSmartDto('filter'), $query, $builder);
         return $this->smartService->from(
@@ -158,7 +159,7 @@ abstract class AbstractRepository extends AbstractMapper implements IRepository 
     }
 
     public function select(array $fields = [], bool $override = false): Query {
-        return $this->queryOf()->select(array_merge([$this->field('$.*')], $fields), $override)->from($this->table);
+        return $this->queryOf()->select(array_merge([$this->field('$.*')], $fields), $override)->distinct($this->field("$.$this->id"))->from($this->table);
     }
 
     public function fetch(Query $query) {

4674ff45d4c9ff3094664ee2bc74ba4c109e9707
Marek Hanzal <marek.hanzal@x32.cz>
2023-08-29 12:49:12 +0200

    ...

--- src/Edde/Dto/Mapper/ExportMapper.php
+++ src/Edde/Dto/Mapper/ExportMapper.php
@@ -9,27 +9,31 @@ use Edde\Mapper\AbstractMapper;
 use Edde\Utils\StringUtils;
 
 class ExportMapper extends AbstractMapper {
-	const META = 'export-mapper';
-	const CONVERT_KEEP = 0;
-	const CONVERT_SNAKE = 1;
+    const META = 'export-mapper';
+    const CONVERT_KEEP = 0;
+    const CONVERT_SNAKE = 1;
 
-	public function item($item, $params = null) {
-		if (!$item) {
-			return null;
-		} else if (!($item instanceof SmartDto)) {
-			throw new SmartDtoException(sprintf('[%s] mapper supports only SmartDto instances (%s); input is [%s].', static::class, SmartDto::class, gettype($item)));
-		}
-		$map = $item->getSchema()->getMeta(static::META, []);
-		$export = [];
-		foreach ($item->export($params['raw'] ?? false) as $k => $value) {
-			$key = $k;
-			switch ($map[$k] ?? false) {
-				case self::CONVERT_SNAKE:
-					$key = StringUtils::recamel($key, '_');
-					break;
-			}
-			$export[$key] = $value;
-		}
-		return (object)$export;
-	}
+    public function item($item, $params = null) {
+        if (!$item) {
+            return null;
+        } else if (!($item instanceof SmartDto)) {
+            throw new SmartDtoException(sprintf('[%s] mapper supports only SmartDto instances (%s); input is [%s].', static::class, SmartDto::class, gettype($item)));
+        }
+        $map = $item->getSchema()->getMeta(static::META, []);
+        $export = [];
+        foreach ($item->export($params['raw'] ?? false) as $k => $value) {
+            $key = $k;
+            switch ($map[$k] ?? false) {
+                case self::CONVERT_SNAKE:
+                    $key = StringUtils::recamel($key, '_');
+                    break;
+                case self::CONVERT_KEEP:
+                    break;
+                default:
+                    $key = $map[$k] ?? $k;
+            }
+            $export[$key] = $value;
+        }
+        return (object)$export;
+    }
 }

65e51da366688e8b6921ee91ef69e2f1a656297e
Marek Hanzal <marek.hanzal@x32.cz>
2023-08-29 12:48:47 +0200

    ...

--- src/Edde/Dto/Mapper/ImportMapper.php
+++ src/Edde/Dto/Mapper/ImportMapper.php
@@ -9,29 +9,33 @@ use Edde\Schema\ISchema;
 use Edde\Utils\StringUtils;
 
 class ImportMapper extends AbstractMapper {
-	const META = 'import-mapper';
-	const CONVERT_KEEP = 0;
-	const CONVERT_CAMEL = 1;
+    const META = 'import-mapper';
+    const CONVERT_KEEP = 0;
+    const CONVERT_CAMEL = 1;
 
-	public function item($item, $params = null) {
-		if (!$item) {
-			return null;
-		} else if (!isset($params['schema'])) {
-			throw new SmartDtoException('Missing [schema => ISchema] parameter.');
-		}
-		/** @var $schema ISchema */
-		$schema = $params['schema'];
-		$map = $schema->getMeta(static::META, []);
-		$export = [];
-		foreach ($item as $k => $value) {
-			$key = $k;
-			switch ($map[$k] ?? false) {
-				case self::CONVERT_CAMEL:
-					$key = StringUtils::toCamelHump($key);
-					break;
-			}
-			$export[$key] = $value;
-		}
-		return $this->smartService->pushOf($export, $schema->getName());
-	}
+    public function item($item, $params = null) {
+        if (!$item) {
+            return null;
+        } else if (!isset($params['schema'])) {
+            throw new SmartDtoException('Missing [schema => ISchema] parameter.');
+        }
+        /** @var $schema ISchema */
+        $schema = $params['schema'];
+        $map = $schema->getMeta(static::META, []);
+        $export = [];
+        foreach ($item as $k => $value) {
+            $key = $k;
+            switch ($map[$k] ?? false) {
+                case self::CONVERT_CAMEL:
+                    $key = StringUtils::toCamelHump($key);
+                    break;
+                case self::CONVERT_KEEP:
+                    break;
+                default:
+                    $key = $map[$k] ?? $k;
+            }
+            $export[$key] = $value;
+        }
+        return $this->smartService->pushOf($export, $schema->getName());
+    }
 }
